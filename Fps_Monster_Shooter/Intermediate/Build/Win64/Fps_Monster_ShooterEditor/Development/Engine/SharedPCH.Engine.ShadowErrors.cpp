5523929d2b09e8894f58b4205039cba3ccccd605
Matthew Sutton <77704375+GearsOfSmores@users.noreply.github.com>
2022-01-16 19:14:11 -0500

    updated first level

--- Fps_Monster_Shooter/Intermediate/Build/Win64/Fps_Monster_ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30706
-#include "C:/Users/ajsut/Documents/Unreal Projects/MonsterShooterFPS/Fps_Monster_Shooter/Intermediate/Build/Win64/Fps_Monster_ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Fps_Monster_Shooter/Source/Fps_Monster_Shooter/Fps_Monster_Shooter.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Fps_Monster_Shooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fps_Monster_Shooter, "Fps_Monster_Shooter" );
- 
